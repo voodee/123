d065ce6f83ea4cd57be898b06cf2bc596292bc67
Roman Arestovich <kidala@xaker.ru>
2021-03-09 12:22:20 +0300

    Changes

---
+++ src/pages/index.js
@@ -0,0 +1,111 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Text, Button } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { GlobalQuarklyPageStyles } from "global-page-styles";
+import { Override, Section } from "@quarkly/components";
+export default (() => {
+	return <Theme theme={theme}>
+		<GlobalQuarklyPageStyles pageUrl={"index"} />
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Section padding="100px 0" sm-padding="40px 0">
+			<Override slot="SectionContent" align-items="center" />
+			<Text as="h2" font="--headline1" md-font="--headline2" margin="20px 0 0 0">
+				About Us
+			</Text>
+			<Text as="p" font="--lead" margin="20px 0 0 0">
+				Hi! I'm a paragraph. Click here to add your own text and edit me. It’s a piece of cake. I’m a great space for you to tell a story and let your site visitors know more about you. Talk about your business and what products and services you offer. Share how you came up with the idea for your company and what makes you different from your competitors. Make your business stand out and show your visitors who you are.
+			</Text>
+			<Button font="--lead" margin="20px">
+				Button
+			</Button>
+			<Text
+				position="absolute"
+				bottom="11.1445px"
+				height="52.18755px"
+				left="829.984px"
+				right="286.97265625px"
+				top="436.65625px"
+				width="163.04294375px"
+			>
+				sdfsdfsdfs
+				<br />
+				g
+				<br />
+				dfg
+				<br />
+				f
+				<br />
+				gdfg
+				<br />
+				dfg
+				<br />
+				fd
+				<br />
+				gfd
+				<br />
+				g
+				<br />
+				fd
+				<br />
+				gf
+				<br />
+				dg
+				<br />
+				fd
+				<br />
+				g
+				<br />
+				fdg
+				<br />
+				df
+				<br />
+				g
+				<br />
+				fd
+				<br />
+				gdf
+				<br />
+				g
+				<br />
+				df
+				<br />
+				gd
+				<br />
+				fg
+				<br />
+				df
+				<br />
+				g
+			</Text>
+		</Section>
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+	</Theme>;
+});
---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5f48e4e3669357001e60f6d4",
+				"5f48e4e3669357001e60f6d6"
+			]
+		},
+		"5f48e4e3669357001e60f6d4": {
+			"id": "5f48e4e3669357001e60f6d4",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5f48e4e3669357001e60f6d6": {
+			"id": "5f48e4e3669357001e60f6d6",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
